d6e9932ee9788640742a9ec06c2584ecfe983ee9
koolpedro <panyakorah@gmail.com>
2023-07-02 10:51:52 -0700

    class tasks

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,15 @@
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+const class2019 = new HolbertonClass(2019, 'San Francisco');
