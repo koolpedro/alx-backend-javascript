4f9d1c9bd9dcf60703f0a3d99a68cb9ec96f6b2a
koolpedro <panyakorah@gmail.com>
2023-07-05 16:02:26 -0700

    getlist task

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   return [
-    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
-    { firstName: 'James', id: 2, location: 'Columbia' },
-    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'serena', location: 'San Francisco' },
   ];
 }
