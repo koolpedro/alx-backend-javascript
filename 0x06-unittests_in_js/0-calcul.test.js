0871752652415ab2d47ab110c2c1eb6319dafd4a
koolpedro <panyakorah@gmail.com>
2023-08-25 19:00:00 -0700

    fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,6 +5,6 @@ describe('calculateNumber', () => {
     it('should calculate the sum of rounded numbers', () => {
         assert.strictEqual(calculateNumber(1.5, 2.7), 5);
         assert.strictEqual(calculateNumber(2.3, 3.8), 6);
+	assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
-
