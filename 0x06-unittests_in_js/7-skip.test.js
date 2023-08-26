b1e7ab84875e7345709a8a0df3e5dfc4d482834f
koolpedro <panyakorah@gmail.com>
2023-08-25 21:47:39 -0700

    fix code

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -1,35 +1,16 @@
 const { expect } = require('chai');
 
-describe('Testing numbers', () => {
-  it('1 is equal to 1', () => {
-    expect(1 === 1).to.be.true;
-  });
+describe('Sample Test Suite', () => {
+    it('should pass a passing test', () => {
+        expect(true).to.be.true;
+    });
 
-  it('2 is equal to 2', () => {
-    expect(2 === 2).to.be.true;
-  });
+    it.skip('should skip a failing test', () => {
+        expect(false).to.be.true; // Failing assertion
+    });
 
-  it.skip('1 is equal to 3', () => {
-    expect(1 === 3).to.be.true;
-  });
-
-  it('3 is equal to 3', () => {
-    expect(3 === 3).to.be.true;
-  });
-
-  it('4 is equal to 4', () => {
-    expect(4 === 4).to.be.true;
-  });
-
-  it('5 is equal to 5', () => {
-    expect(5 === 5).to.be.true;
-  });
-
-  it('6 is equal to 6', () => {
-    expect(6 === 6).to.be.true;
-  });
-
-  it('7 is equal to 7', () => {
-    expect(7 === 7).to.be.true;
-  });
+    it('should pass another passing test', () => {
+        expect(2 + 2).to.equal(4);
+    });
 });
+
